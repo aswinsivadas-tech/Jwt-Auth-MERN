fa8e33f2fc69274372e169aaf058b61b36b9f8aa
aswinsivadas-tech <aswinsivadas.tech@gmail.com>
2026-05-12 13:05:33 +0530

    init: expresss server

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "http://localhost:5176",
+      "http://localhost:5177",
+    ], // Allow multiple local ports
+    credentials: true,
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
+// Routes
+import authRoutes from "./routes/authRoutes.js";
+app.use("api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
